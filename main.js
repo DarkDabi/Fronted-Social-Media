4ac8b12be5c55c987564430bfe7e02846e96e353
DarkDabi <105359598+DarkDabi@users.noreply.github.com>
2024-03-15 13:08:40 +0100

    updated

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-var image = document.getElementById('image1');
-var video = document.getElementById('video1');
-var videoContainer = document.getElementById('videoContainer');
-var mouseOverImage = false;
+// var image = document.getElementById('image1');
+// var video = document.getElementById('video1');
+// var videoContainer = document.getElementById('videoContainer');
+// var mouseOverImage = false;
 
-video.style.display = 'none'; // Initially hide the video
+// video.style.display = 'none'; // Initially hide the video
 
-videoContainer.addEventListener('mouseenter', function() {
-    mouseOverImage = true;
-    setTimeout(function() {
-        if (mouseOverImage) {
-            image.style.display = 'none';
-            video.style.display = 'block';
-            video.play();
-        }
-    }, 1000);
-});
+// videoContainer.addEventListener('mouseenter', function() {
+//     mouseOverImage = true;
+//     setTimeout(function() {
+//         if (mouseOverImage) {
+//             image.style.display = 'none';
+//             video.style.display = 'block';
+//             video.play();
+//         }
+//     }, 1000);
+// });
 
-videoContainer.addEventListener('mouseleave', function() {
-    mouseOverImage = false;
-    video.pause();
-    video.currentTime = 0;
-    image.style.display = 'block';
-    video.style.display = 'none';
-});
+// videoContainer.addEventListener('mouseleave', function() {
+//     mouseOverImage = false;
+//     video.pause();
+//     video.currentTime = 0;
+//     image.style.display = 'block';
+//     video.style.display = 'none';
+// });
+class Video{
+    id;
+    data; 
+    image;
+    title;
+    time;
+    username;
+}
+fetch("./MOCK_DATA.json")
+.then(response => response.json())
+.then(data =>{console.log(data);
+   let post = new Video(data.name, data.img)
+   
+       document.createHTML('Video');
+  
+       Elemental.render(document.getElementById('dataContainer'), data);
+   })
+   .catch(error => console.error('Error fetching JSON:', error));
